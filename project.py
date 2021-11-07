1918ccb2785c3681f6add4620716ad6e92720ea8
TanishiDesai <72441998+TanishiDesai@users.noreply.github.com>
2021-11-07 22:23:28 +0530

    Update project.py

--- project.py
+++ project.py
@@ -10,7 +10,7 @@ file_data.pop(0)
 new_data=[]
 for i in range(len(file_data)):
 	n_num = file_data[i][2]
-	new_data.append(n_num)
+	new_data.append(float(n_num))
 
 n = len(new_data)
 new_data.sort()
@@ -18,7 +18,7 @@ new_data.sort()
 def mean() :
     total = 0
     for x in new_data:
-        total += number(x)
+        total += (x)
     mean = total / n
     print("Mean / Average is: " + str(mean)) 
     
@@ -33,28 +33,52 @@ def mean() :
 #         median = new_data[n//2]
 #     print("Median is: " + str(median))
 
-# def mode() :
-#     data = Counter(new_data)
-#     mode_data_for_range = { 
-#         "75 - 85" : 0,
-#         "85 - 95" : 0,
-#         "95 - 105" : 0,
-#     }
-#     for weight, orrurence in data.items():
-#         if 75 < float(weight) < 85:
-#             mode_data_for_range["75-85"] += occurence
-#         elif 85 < float(weight) < 95:
-#             mode_data_for_range["85-95"] += occurence
-#         elif 95 < float(weight) < 105:
-#             mode_data_for_range["95-105"] += occurence
-#     mode_range, mode_occurence = 0,0
-#     for range, occurence in mode_data_for_range.items():
-#         if occurence > mode_occurence:
-#             mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
-#     mode = float((mode_range[0] + mode_range[1]) / 2)
-#     print(f"Mode is -> {mode:2f}")
+def mode() :
+    data = Counter(new_data)
+    mode_data_for_range = { 
+        "75 - 85" : 0,
+        "85 - 95" : 0,
+        "95 - 105" : 0,
+        "105 - 115" : 0,
+        "115 - 125" : 0,
+        "125 - 135" : 0,
+        "135 - 145" : 0,
+        "145 - 155" : 0,
+        "155 - 165" : 0,
+        "165 - 175" : 0
+    }
+    for weight, occurence in data.items():
+        if 75 < (weight) < 85:
+            mode_data_for_range["75-85"] += occurence
+        elif 85 < (weight) < 95:
+            mode_data_for_range["85-95"] += occurence
+        elif 95 < (weight) < 105:
+            mode_data_for_range["95-105"] += occurence
+        elif 105 < (weight) < 115:
+            mode_data_for_range["105-115"] += occurence
+        elif 115 < (weight) < 125:
+            mode_data_for_range["115-125"] += occurence
+        elif 125 < (weight) < 135:
+            mode_data_for_range["125-135"] += occurence
+        elif 135 < (weight) < 145:
+            mode_data_for_range["135-145"] += occurence
+        elif 145 < (weight) < 155:
+            mode_data_for_range["145-155"] += occurence
+        elif 155 < (weight) < 165:
+            mode_data_for_range["155-165"] += occurence
+        elif 165 < (weight) < 175:
+            mode_data_for_range["165-175"] += occurence
+    mode_range, mode_occurence = 0,0
+    for range, occurence in mode_data_for_range.items():
+        if occurence > mode_occurence:
+            mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+    mode = float((mode_range[0] + mode_range[1]) / 2)
+    print(f"Mode is -> {mode:2f}")
 
-mean()
+# mean()
+# median()
+mode()
+
 
 
 
